c2c109e9b0c2d2e8febf1ad34a389f2933bc0c5f
Adrian Brandemuehl <abrandemuehl@gmail.com>
2022-04-01 18:16:42 +0200

    Fix worker gpu containers and name them kinetic

--- build-containers.sh
+++ build-containers.sh
@@ -3,10 +3,10 @@
 echo "BUILDING API SERVER CONTAINER..."
 docker build -t amzracing/rbb-api-server:latest -f deploy/api-server.docker .
 
-echo "\n\nBUILDING NO-GPU WORKER CONTAINER..."
-docker build -t amzracing/rbb-tools:latest -f deploy/worker-no-gpu.docker .
+echo "\n\nBUILDING KINETIC NO-GPU WORKER CONTAINER..."
+docker build -t amzracing/rbb-tools:kinetic -f deploy/worker-kinetic-no-gpu.docker .
 
-#echo "\n\nBUILDING NVIDIA|OPENGL|ROS CONTAINER..."
-#docker build -t ros-kinetic-desktop-nvidia-opengl:latest -f deploy/docker-ros-kinetic-desktop-nvidia.docker .
-#echo "\n\nBUILDING GPU WORKER CONTAINER..."
-#docker build -t amzracing/rbb-tools-gpu-nvidia:latest -f deploy/worker.docker .
+echo "\n\nBUILDING NVIDIA|OPENGL|ROS CONTAINER..."
+docker build -t ros-kinetic-desktop-nvidia-opengl:latest -f deploy/docker-ros-kinetic-desktop-nvidia.docker .
+echo "\n\nBUILDING GPU WORKER CONTAINER..."
+docker build -t amzracing/rbb-tools-gpu-nvidia:latest -f deploy/worker-kinetic-gpu.docker .
